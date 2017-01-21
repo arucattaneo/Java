272375e72956a4e67cbb9ea6cb4cddaccf5a73ea
educacionit <educacionit@alumno-802-03>
2017-01-21 13:39:42 -0300

    primer edit

--- src/proyectointegrador1/Programa.java
+++ src/proyectointegrador1/Programa.java
@@ -15,6 +15,7 @@ public class Programa {
         Vehiculo v1= new Vehiculo(20,10,50);
         System.out.println("ancho: "+ v1.getAncho()+", alto: " + v1.getAlto()+ ", largo: "+v1.getLargo());
         Persona p1=new Persona("Juan","Sanchez","39770683");
+        
         System.out.println("Nombre: "+p1.getNormbre()+", Apellido: "+p1.getApellido()+", DNI: "+ p1.getNumeroDocumento());
     }
 }
